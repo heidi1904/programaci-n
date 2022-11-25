03c708e9b669d314a0098c9b798988d5c80d5b01
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-11-25 17:41:30 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,37 +1,32 @@
-#$ pip install streamlit --upgrade 
 import streamlit as st
 import pandas as pd
 import numpy as np
-import urllib.request
-from streamlit_option_menu import option_menu
+import gdown
 
-with st.sidebar: 
-    st.markdown("###")
-    st.sidebar.header('Programación Avanzada')
-    selected = option_menu(
-        menu_title = 'Menú',
-        options = ['Inicio', 'Reporte','Equipo'],
-        icons = ['house', 'book', 'book','people'],
-        menu_icon='cast',
-        default_index = 0,
-    )
+import pip
+pip.main(["install", "openpyxl"])
+pip.main(["install", "pandas"])
+import plotly.figure_factory as ff
+pip.main(["install", "matplotlib"])
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.figure_factory as ff
+import scipy
 
-if selected == 'Inicio':
-  st.markdown("<h1 style ='text-align: center'>Titulo:</h1>", unsafe_allow_html=True)
-  st.markdown("---")
-  st.header("Dataset")
-  
-  @st.experimental_memo
-  def download_data():
-        url ="https://raw.githubusercontent.com/heidi1904/programaci-n/main/Catalogo.xlsx%20-%20Catalogo1960_2021.csv"
-        filename ="Catalogo.xlsx%20-%20Catalogo1960_2021.csv"
-        urllib.request.urlretrieve(url,filename)
-        df = pd.read_csv('Catalogo.xlsx%20-%20Catalogo1960_2021.csv')
-        return df
-  download_data()
-  st.dataframe(download_data())
-    
-if selected == 'Reporte':
-    st.markdown("<h1 style ='text-align: center'>Reporte:</h1>", unsafe_allow_html=True)
+st.title("Catálogo sísmico 1960-2021")
+df_cat = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx', header= 0) 
+st.write(df_cat)
+st.header("Sismos ocurridos en el Perú para el período 1960-2021")
+st.write("Esta base de datos sísmicos contiene todos los parámetros que caracterizan a un sismo, calculados en las mismas condiciones a fin de constituirse como una base homogénea: fecha, hora, latitud, longitud, profundidad y magnitud. En este dataset se podrá encontrar el Catálogo de Sismos Instrumentales para el período de 1960 – 2021.")
+
+option = st.selectbox('¿Si desea más información puede contactarnos mediante las siguientes opciones?',('Email', 'Teléfono', 'Whatsapp', 'Instagram'))
+st.write('Seleccionó:', option)
+
+st.header("Histogramas de datos sísmicos:")
+for i in range(5,7):
+    fig = px.histogram(df_cat, df_cat.columns[i])
+    st.plotly_chart(fig, use_container_width=True)
   
   
