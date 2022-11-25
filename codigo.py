642a75ceb74625ad1ec8edba605551fcb2d83c73
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-11-25 18:03:22 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import urllib.request
 from streamlit_option_menu import option_menu
+import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.figure_factory as ff
 #import scipy
