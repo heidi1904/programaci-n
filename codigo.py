aaa7e7ff14ccd7d57d52a7a8440306ce5dd96646
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-11-25 15:49:29 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,12 +4,16 @@ import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 
-with st.sidebar:
-  selected = option_menu(
-    menu_title='Menu',
-    options =['Inicio', 'Reporte','Equipo'],
-    icons=['house', 'book', 'people'],
-    menu_icon='cast',
-    default_idex=0,
-  )
-   
+with st.sidebar: 
+    st.markdown("###")
+    st.sidebar.header('Programación Avanzada')
+    selected = option_menu(
+        menu_title = 'Menú',
+        options = ['Inicio', 'Reporte','Equipo'],
+        icons = ['house', 'book', 'book','people'],
+        menu_icon='cast',
+        default_index = 0,
+    )
+
+if selected == 'Inicio':
+  st.markdown("<h1 style ='text-align: center'>Titulo:</h1>", unsafe_allow_html=True)
