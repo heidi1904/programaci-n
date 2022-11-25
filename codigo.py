b6b1fddcf88cb6ab5f90a6e552c97cbc020249bc
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-11-25 14:11:16 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,8 +5,8 @@ import numpy as np
 import streamlit_option_menu import option_menu
 
 with st.sidebar:
-  selected=option_menu(
-    mentu_title='Menu',
+  selected = option_menu(
+    menu_title='Menu',
     options =['Inicio', 'Reporte','Equipo'],
     icons=['house', 'book', 'people'],
     menu_icon='cast',
