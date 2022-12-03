51a916ec81da963602df34db111c5a7c5566a672
Nicole Gutierrez <117460418+NickyTCV@users.noreply.github.com>
2022-12-03 12:17:23 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -64,6 +64,7 @@ if selected == 'Mapas':
     image = Image.open('Image_Dr.jpg')
     st.image(image)
     st.write("**Fuente:** Instituto Geofísico del Perú")
+    df_local=pd.read_csv("https://raw.githubusercontent.com/heidi1904/programaci-n/main/Catalogo.xlsx%20-%20Catalogo1960_2021.csv")
     @st.cache
     def localizacion_data():
         df_local = pd.read_csv('Catalogo.xlsx%20-%20Catalogo1960_2021.csv')
