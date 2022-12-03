9e3c43788865a00bc843f31d9f1806708272b24b
Nicole Gutierrez <117460418+NickyTCV@users.noreply.github.com>
2022-12-02 20:45:27 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -41,8 +41,7 @@ if selected == 'Inicio':
     download_data()
     df_cat = pd.read_csv('Catalogo.xlsx%20-%20Catalogo1960_2021.csv') 
     st.dataframe(df_cat)
-   
-    st. header("Sismos ocurridos en el Perú para el período 1960-2021")
+    st.subheader("Sismos ocurridos en el Perú para el período 1960-2021")
     st.write("Esta base de datos sísmicos contiene todos los parámetros que caracterizan a un sismo, calculados en las mismas condiciones a fin de constituirse como una base homogénea: fecha, hora, latitud, longitud, profundidad y magnitud. En este dataset se podrá encontrar el Catálogo de Sismos Instrumentales para el período de 1960 – 2021.")
     st . header ( "Histogramas de datos sísmicos:" )
     for i in range(5,7):
