e786cd5f98392095a4c964e06bde0cd50b9c4375
heidi1904 <117460221+heidi1904@users.noreply.github.com>
2022-12-04 00:45:57 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,7 +1,7 @@
 #$ pip install streamlit --upgrade 
 import streamlit as st
 import pandas as pd
-import numpy as np
+import numpy as np 
 import urllib.request
 from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
